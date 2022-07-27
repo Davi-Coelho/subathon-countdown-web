56897e5e997fabadc92cc2939d58aee72e3be496
Davi Coelho <davi.cmbarreto@gmail.com>
2022-07-27 00:29:36 -0400

    configuração do servidor e do websocket

---
+++ app.js
@@ -0,0 +1,6 @@
+const app = require('./config/server')
+const appWs = require('./config/ws')
+const port = 3005
+
+const server = app.listen(port, () => console.log(`App Express is running on port ${port}`))
+app.appWs = appWs(server)
