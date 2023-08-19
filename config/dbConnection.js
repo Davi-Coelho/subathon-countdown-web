92c6f29dfb7c7ed5c5d2d8fd6ee2b83c618c9117
Davi Coelho <davi.cmbarreto@gmail.com>
2023-08-18 22:28:22 -0400

    att: url do banco

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -30,7 +30,7 @@ const ConfigModel = mongoose.model('subathon_config', configSchema)
 
 async function initDatabase() {
     try {
-        const url = ENV !== 'dev' ? `mongodb://${DB_USER}:${DB_PASS}@mongo-0.mongo,mongo-1.mongo/${DB}?authSource=admin` : 'mongodb://localhost:27017/subathontimer'
+        const url = ENV !== 'dev' ? `mongodb://${DB_USER}:${DB_PASS}@mongo-0.mongo/${DB}?authSource=admin` : 'mongodb://localhost:27017/subathontimer'
         console.log(url)
         await mongoose.connect(url)
         console.log('Conectado ao banco de dados!')
